35d35ab2ba4f233af5896b03949fb6193b379284
Yanick Theodose <yanick.theodose@outlook.com>
2020-08-12 18:42:33 +0200

    "insertion requete sql"

--- dialogue.php
+++ dialogue.php
@@ -2,8 +2,11 @@
 $mysqli = new Mysqli('localhost','root','', 'dialogue');
 if($_POST)
 {
-	echo "pseudo posté: $_POST[pseudo]<br>";
-	echo "message posté: $_POST[message]<br>";
+	//echo "pseudo posté: $_POST[pseudo]<br>";
+	//echo "message posté: $_POST[message]<br>";
+	$mysqli->query("INSERT INTO commentaire(pseudo, message, date_enregistrement) VALUES ('$_POST[pseudo]', '$_POST[message]', NOW())")
+			OR DIE ($mysqli->error);
+	echo "<div class='validation'>Votre message a bien été enregistré.</div>";
 }
 ?>
 <hr>
